435afa72faaff97f453d2efde668b0bc9ab3776d
Jovana Agobian <jovana.agobian@gmail.com>
2022-05-12 18:14:01 -0400

    "Phase preview and css changes"

--- src/components/Phase1.js
+++ src/components/Phase1.js
@@ -1,12 +1,34 @@
-import React from "react";
+import { click } from "@testing-library/user-event/dist/click";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Phase1({ resources }) {
+   
+    const [clickedLink, setClickedLink] = useState("")
+
     const phase1 = resources.filter((resource) => resource.phase === 1)
-    const render = phase1.map((data) => <Link className="Phase" to="/phase1">{data.title}</Link> )
+    const phase1Resources = phase1.filter((resource) => resource.title === clickedLink) 
+    const render = phase1.map((data) => <Link className="Phase" to="/phase1">{data.title} </Link> )
+    //console.log(phase1)
+
+        
+
+        function handleClick(e) {
+            setClickedLink(e.target.innerText)
+            
+        }
+        
     return (
-        <div>
-            {render}
+        <div className="Preview">
+              {clickedLink ? 
+                        <>
+                            <p>{phase1Resources[0].title}</p>
+                            <p>{phase1Resources[0].description}</p> 
+                        </> :
+                null}
+                <p onClick={(e) => handleClick(e)}>{render}</p>
+            
+                
         </div>
 )}
 export default Phase1;
