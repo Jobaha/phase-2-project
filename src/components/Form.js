4dc1609de1b5c4a79477e34906cf28ff7625864c
Deven <devenzachuth@gmail.com>
2022-05-12 17:15:50 -0500

    "Form is 100% working"

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,52 +1,42 @@
 import React, {useState} from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 function Form( {addNewItem} ) {
 
     const [title, setTitle] = useState('')
     const [url, setUrl] = useState('')
     const [description, setDescription] = useState('')
-    const [phase, setPhase] = useState(0)
+    const [phase, setPhase] = useState('')
 
-    const handleSubmit = (e) => {
+
+    function handleSubmit(e) {
         e.preventDefault()
-
-        const obj = {
-            phase: phase,
-            title: title,
-            link: url,
-            description: description
-        }
-        console.log(obj)
-
         fetch(`${process.env.REACT_APP_API_URL}/flatsource`, {
             method: "POST",
             headers: {
-              "Content-Type": "application/json"
+              "Content-Type": "application/json",
             },
-            body: JSON.stringify(obj)
+            body: JSON.stringify( {
+            id: uuidv4(),
+            phase: parseInt(phase),
+            title: title,
+            link: url,
+            description: description
+            }),
         })
         .then(res => res.json())
-        .then(console.log)
-        // .then(newItem => addNewItem(newItem))
-
-        setTitle('')
-        setUrl('')
-        setDescription('')
-        setPhase(0)
+        .then(newItem => addNewItem(newItem));
     }
+
 
     return (
         <div className="form-add">
             <h2>Create new Resources here!</h2>
             <form className="full-form" onSubmit={handleSubmit}>
-                <input type='text' placeholder="Title" value={title} onChange={e => setTitle(e.target.value)}></input>
-                <br></br>
-                <input type='text' placeholder="URL" value={url} onChange={e => setUrl(e.target.value)}></input>
-                <br></br>
-                <input type='text' placeholder="Description" value={description} onChange={e => setDescription(e.target.value)}></input>
-                <br></br>
-                Phase Number <input type='number' max='5' min='0' value={phase} onChange={e => setPhase(e.target.value)}></input>
-                <br></br>
+                <input type='text' placeholder="Title..." width='' value={title} onChange={e => setTitle(e.target.value)}></input><br></br>
+                <input type='text' placeholder="URL..." value={url} size='50' onChange={e => setUrl(e.target.value)}></input><br></br>
+                <input type='text' placeholder="Description..." size='50' value={description} onChange={e => setDescription(e.target.value)}></input><br></br>
+                Phase Number: <input type='number' max='5' min='1' maxLength='1' value={phase} onChange={e => setPhase(e.target.value)}></input><br></br>
                 <button type="submit">Add Resource</button>
             </form>
         </div>
